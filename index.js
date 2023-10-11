9a535a12062f92dcdc05956d2cb288d760c97f9a
Katie Barrett <katiebarrett@Katies-MacBook-Air.local>
2023-10-11 16:09:07 -0600

    project complete

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const PORT = 8080;
 
 // GET - / - returns homepage
 app.get('/', (req, res) => {
-    // serve up the public folder as static index.html file
-
+    res.sendFile(__dirname + "/public/index.html");
 });
 
 // hello world route
@@ -19,32 +18,47 @@ app.get('/api', (req, res) => {
 });
 
 // get all pets from the database
+// to do this, we just need to access the data which has already been set to pets in line 2
+// so all we have to do is res.send(pets)
 app.get('/api/v1/pets', (req, res) => {
-    // send the pets array as a response
-
+    //   console.log(pets);
+    res.send(pets);
 });
 
 // get pet by owner with query string
+// we are querying the owners name so 
+// when api/v1/pets?owner=John, it will show Johns pets
+//However, john has more than one pet so I am actually going to change the find all pet in the pets array 
+//in order to display all of Johns pets 
+//I will do this by using a pets.filter instead of pet.find
+//Pet.find was only showing John's first pet Fido, not his second pet Rover
+
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
-
+    const { owner } = req.query;
 
     // find the pet in the pets array
-    const pet = pets.find(pet => pet.owner === owner);
+    const matchingPets = pets.filter(pet => pet.owner === owner);
 
     // send the pet as a response
-
+    res.send(matchingPets);
 });
 
 // get pet by name
+// here, we are looking to get the pet based on their name that is in the url
+//to do this we will use req.params to get the name
+// then find that pet based off what pet has that pet.name in the data
+//then we will send that pet as a response
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
+    const { name } = req.params;
 
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
 
     // send the pet as a response
+    res.send(pet);
 
 });
 
